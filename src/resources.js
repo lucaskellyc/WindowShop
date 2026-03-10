634e241328cb2c8bf3be2728dd1ae77ed78d65b8
Kelly <kelly@Mac-912.lan>
2026-03-10 19:45:25 -0400

    Add new 3D asset files and update HTML for script loading.

--- src/resources.js
+++ src/resources.js
@@ -1,7 +1,8 @@
-export class ResourceSet {
+class ResourceSet {
   constructor() {
     this.materials = [];
     this.textures = [];
+    this.videos = [];
   }
   load(loader, assets) {
     for (let i = 0; i < assets.textures.length; i++) {
@@ -13,6 +14,14 @@ export class ResourceSet {
         this.textures[i].items[index].type = item.type;
         this.textures[i].items[index].texture = await loader.load(item.src);
       });
+    }
+    for (let i = 0; i < assets.videos.length; i++) {
+      this.videos[i] = {};
+      this.videos[i].name = assets.videos[i].name;
+      this.videos[i].video = document.createElement("video");
+      this.videos[i].video.src = assets.videos[i].src;
+      this.videos[i].video.loop = true;
+      this.videos[i].video.muted = true;
     }
   }
 }
